697a7f412cfa983d6f84a8c70a4935ce06f15eaa
jamesrascal <roger.j.gentry@gmail.com>
2014-07-14 21:34:44 -0700

    Outside HTML DIR support MySQL Gzip
    Script now checks that wp-config.php exists before going forward. If the script finds that the directory is missing then it checks the directory above wp_root. 
    
    MySQL dump is now a Gzipped.

--- WordPressBackup.sh
+++ WordPressBackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author Roger Gentry (jamesrascal) - Host Kraken
-# WP BACKUPS V1.8
+# WP BACKUPS V1.9
 # Build date 07/14/2014
 
 # Adjust this to where you websites are stored.
@@ -13,15 +13,33 @@ for backupprofile in $profile ; do
 echo "********************************************************************";
 echo "Using Profile: ${backupprofile}";
 . $backupprofile
-                if [ "${backup_enabled}" = "1" ]; then
-                                # BackupName Date and time
+                
+	if [ "${backup_enabled}" = "1" ]; then
+                        wp_config=${wp_root}/wp-config.php
+                                # Verifing that wp-config is in the location
+		  if [ ! -f "$wp_config" ]; then
+		   echo "No WP-Config.php Found Attempting to find one";
+	                   # Checks the directory above WP_root
+		   cd $wp_root
+		   wp_config=../wp-config.php
+                                   # IF wp-configstill not found
+                                   if [ ! -f "$wp_config" ]; then
+		    echo "FATAL ERROR: wp-config.php is missing from a readable state";
+		      echo "Does $wp_domain still work?";
+				 
+		     fi
+		fi
+                                
+                                # Verifing wp_config exists
+		if [ -f "$wp_config" ]; then
+	                # BackupName Date and time
                                 backupname=$(date +%m%d%y)
-                                echo "Backing up WordPress site at ${wp_root}";
+	
                                 # Pulls Database info from WP-config
-                                db_name=$(grep DB_NAME "${wp_root}/wp-config.php" | cut -f4 -d"'")
-                                db_user=$(grep DB_USER "${wp_root}/wp-config.php" | cut -f4 -d"'")
-                                db_pass=$(grep DB_PASSWORD "${wp_root}/wp-config.php" | cut -f4 -d"'")
-                                table_prefix=$(grep table_prefix "${wp_root}/wp-config.php" | cut -f2 -d"'")
+                                db_name=$(grep DB_NAME "${wp_config}" | cut -f4 -d"'")
+                                db_user=$(grep DB_USER "${wp_config}" | cut -f4 -d"'")
+                                db_pass=$(grep DB_PASSWORD "${wp_config}" | cut -f4 -d"'")
+                                table_prefix=$(grep table_prefix "${wp_config}" | cut -f2 -d"'")
 
                                 # Creates a Backup Directory if one does not exist.
                                 mkdir -p ${backup_location}/${user}/
@@ -32,11 +50,11 @@ echo "Using Profile: ${backupprofile}";
 
 
                                 # MySQL Takes a Dump and compress the Home Directory
-                                mysqldump -u ${db_user} -p${db_pass} ${db_name} > ./${backupname}-DB.sql &&
+                                mysqldump -u ${db_user} -p${db_pass} ${db_name} | gzip > ./${backupname}-DB.sql.gz &&
                                 tar zcPf ./${backupname}-FILES.tar.gz ${wp_root}
 
                                 # Compresses the MySQL Dump and the Home Directory
-                                tar zcPf ./${wp_domain}-${backupname}.tar.gz ./${backupname}-FILES.tar.gz ./${backupname}-DB.sql
+                                tar zcPf ./${wp_domain}-${backupname}.tar.gz ./${backupname}-FILES.tar.gz ./${backupname}-DB.sql.gz
 
                                 # Generates the Backup Size
                                 FILENAME=${backup_location}/${user}/${wp_domain}/${wp_domain}-${backupname}.tar.gz
@@ -48,7 +66,8 @@ echo "Using Profile: ${backupprofile}";
 
                                 #Deletes any Backup older than X days
                               find ${backup_location}/${user}/${wp_domain}/ -type f -mtime +${keepdays} -exec rm {} \;
-                fi
+                	fi
+		fi
                 if [ "${backupenabled}" = "0" ]; then
                                 echo "Backups NOT enabled for ${wp_root}";
                 fi
